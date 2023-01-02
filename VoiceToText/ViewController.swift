73513e4321cea391ef7c4dc531f6859bd86b5ce2
Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2023-01-02 17:50:45 +0530

    Initial Commit

---
+++ VoiceToText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceToText
+//
+//  Created by Mekala Vamsi Krishna on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
